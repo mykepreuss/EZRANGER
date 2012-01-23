c2cb90cd145c47d178062a3419bc633262c5096e
Michael Preuss <myke@waytoogood.ca>
2012-01-23 12:58:37 -0800

    adding second function

--- js/global.js
+++ js/global.js
@@ -8,11 +8,25 @@ $(function() {
 		var y = parseInt($('#hp').val());
 		var estimate = (x + (Math.sqrt(x + y))).toFixed(1);
 		var time = $('#time').val();
-		$('#estimate').html(estimate+' '+time).fadeIn();
+		$('#estimate').html(estimate+' '+time);
 	}
+
+	function estimate2(){
+		var x = parseInt($('#abp').val());
+		var y = parseInt($('#hp').val());
+		var bufferRoot = Math.pow((y - x),2);
+		console.log(bufferRoot);
+		var buffer = Math.sqrt(bufferRoot);
+		console.log(buffer);
+		var estimate = (buffer + x).toFixed(1);
+		var time = $('#time').val();
+		$('#estimate2').html(estimate+' '+time);
+	}
+
 		
 	$('#calculate').click(function(){
 		estimate();
+		estimate2();
 		return false;
 	});
 });
