0cbab7abdb48ccdb5e6d3d0b5f3f0fe2f4470ce2
Michael Preuss <myke@waytoogood.ca>
2012-01-23 10:42:51 -0800

    creating funtion

--- js/global.js
+++ js/global.js
@@ -1,7 +1,21 @@
 /* Author:
-
+	@mykepreuss
 */
 
+$(function() {
+	function estimate(){
+		var x = parseInt($('#abp').val());
+		var y = parseInt($('#hp').val());
+		var estimate = x + (Math.sqrt(x + y));
+		var time = $('#time').val();
+		$('#estimate').html(estimate+' '+time);
+	}
+		
+	$('#calculate').click(function(){
+		estimate();
+		return false;
+	});
+});
 
 
 
