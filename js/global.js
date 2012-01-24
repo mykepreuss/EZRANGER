281b82afdcdd6d3ebc95141c5d66267a12b5fa1c
Michael Preuss <myke@waytoogood.ca>
2012-01-24 10:26:04 -0800

    adding multiple inputs

--- js/global.js
+++ js/global.js
@@ -4,31 +4,36 @@
 
 $(function() {
 	function estimate(){
-		var x = parseInt($('#abp').val());
-		var y = parseInt($('#hp').val());
-		var estimate = (x + (Math.sqrt(x + y))).toFixed(1);
+		//Add up all the ABP's
+		abp = 0;	
+		$('.abp:input').each(function() {
+			abp += parseInt($(this).val());
+		});
+		
+		//Add up all the HP's	
+		hp = 0;	
+		$('.hp:input').each(function() {
+			hp += parseInt($(this).val());
+		});
+ 			
+		var bufferRoot = Math.pow((hp - abp),2);
+		var buffer = Math.sqrt(bufferRoot);
+		var estimate = (buffer + abp).toFixed(1);
 		var time = $('#time').val();
 		$('#estimate').html(estimate+' '+time);
-	}
-
-	function estimate2(){
-		var x = parseInt($('#abp').val());
-		var y = parseInt($('#hp').val());
-		var bufferRoot = Math.pow((y - x),2);
-		console.log(bufferRoot);
-		var buffer = Math.sqrt(bufferRoot);
-		console.log(buffer);
-		var estimate = (buffer + x).toFixed(1);
-		var time = $('#time').val();
-		$('#estimate2').html(estimate+' '+time);
-	}
+	};
 
 		
 	$('#calculate').click(function(){
 		estimate();
-		estimate2();
 		return false;
 	});
+	
+	// Add More Tasks Button	
+	$('#addTask').click(function(){
+		var newTask = '<div class="task"><p><label name="abp">ABP:</span><input type="text" name="abp" class="abp" /></p><p><label name="abp">HP:</span><input type="text" name="hp" class="hp" /></p></div>';
+		$(newTask).hide().appendTo('#form').fadeIn();
+	})
 });
 
 
