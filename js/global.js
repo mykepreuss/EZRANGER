35227c3705e30cfe71374a1f82342da3c0c08ff7
Myke <myke@waytoogood.ca>
2012-02-13 18:51:35 -0800

    finally returning the values of each input field

--- js/global.js
+++ js/global.js
@@ -4,56 +4,64 @@
 
 $(function() {
 	function estimate(){
-		//Add up all the ABP's
-		abp = 0;	
+		// Add up all the ABP's
+		var abp = 0;
 		$('.abp:input').each(function() {
 			abp += parseInt($(this).val());
 		});
-		
-		//Add up all the HP's	
-		hp = 0;	
+
+		// Add up all the HP's
+		var hp = 0;
 		$('.hp:input').each(function() {
 			hp += parseInt($(this).val());
 		});
- 		
- 		//THIS IS WHERE I'M HAVING TROUBLE
+		
+		// Make a variable to hold the difference in our estimates
+		var tasks = [];
+
  		$('.task').each(function(){
- 			abp2 = parseInt($(this).find('.abp:input').val());
- 			
- 			console.log(abp2);
+ 			var abp = parseInt($(this).find('.abp:input').val());
+ 			var hp = parseInt($(this).find('.hp:input').val());
+ 			var task =  hp - abp;
+ 			//Fill the tasks[] variable with the differences of each value
+			tasks.push(task);
+ 		});
 
- 			hp2 = parseInt($(this).find('.hp:input').val());
-
- 			console.log(hp2);
-
- 			task =  hp2 - abp2;
+ 		console.log("tasks = " + tasks);
+		
+		// Assign the total variable as zero
+		var total = 0;
+		
+		// Add up all of the task values
+		for (var i = 0; i< tasks.length; i++) {
+			total += tasks[i];
+		}
 			
-			// How do I get the values of task transferred out of this each function so I can use the numbers in the next part of the equation?
-			
- 			console.log(task);
- 			
- 		})
- 		 		
-		var bufferPower = Math.pow((hp - abp),2);
+		// Math time!
+		var bufferPower = Math.pow((total),2);
 		var bufferRoot = Math.sqrt(bufferPower);
+
+ 		console.log("total = " + total);
+ 		console.log("abp = " + abp);
+ 		console.log("hp = " + hp);
+
+ 		console.log("bufferPower = " + bufferPower);
+ 		console.log("bufferRoot = " + bufferRoot);
+
 		var estimate = (bufferRoot + abp).toFixed(1);
 		var time = $('#time').val();
+
 		$('#estimate').hide().html(estimate+' '+time).fadeIn();
 	};
 
-		
 	$('#calculate').click(function(){
 		estimate();
 		return false;
 	});
-	
-	// Add More Tasks Button	
+
+	// Add More Tasks Button
 	$('#addTask').click(function(){
 		var newTask = '<div class="task"><p><label name="abp">ABP:</span><input type="text" name="abp" class="abp" /></p><p><label name="abp">HP:</span><input type="text" name="hp" class="hp" /></p></div>';
 		$(newTask).hide().appendTo('#form').fadeIn();
 	})
 });
-
-
-
-
