8f90d331e32308b3d3a68b91f338da5977eaf24b
Myke <myke@waytoogood.ca>
2012-02-23 23:43:32 -0800

    adding preliminary styling

--- js/global.js
+++ js/global.js
@@ -20,14 +20,14 @@ function estimate(){
 		// Check to see if the ABP is lower than HP
 		if(hp < abp ){
 			tasks.push('tooHigh');
-			$(this).addClass('error');
+			$(this).addClass('alert alert-block');
 		// Check to see if either of the ABP or HP is emtpy
 		} else if (abp === 0 || hp === 0){
-			$(this).addClass('error');
+			$(this).addClass('alert alert-block');
 		} else {
 			// Fill the tasks[] variable with the differences of each value
 			tasks.push(task);
-			$(this).removeClass('error');
+			$(this).removeClass('alert alert-block');
 		}		
 	});
 	
@@ -46,11 +46,11 @@ function estimate(){
 
 
 	if ($.inArray('tooHigh', tasks) > -1){
-		$('#estimate').hide().html('Make sure all ABP values are lower than HP values').fadeIn();	
+		$('#estimate').hide().html('<div class="alert alert-error">Make sure all ABP values are lower than HP values</div>').fadeIn();	
 	} else if (estimate === 'NaN'){
-		$('#estimate').hide().html('Make sure to enter numbers for all fields').fadeIn();
+		$('#estimate').hide().html('<div class="alert alert-error">Make sure to enter numbers for all fields</div>').fadeIn();
 	} else {
-		$('#estimate').hide().html('<div class="estimateTotal">'+estimate+'</div> <div class="time">'+time+'</div>').fadeIn();
+		$('#estimate').hide().html('<div class="alert alert-success"><div class="estimateTotal">'+estimate+'&nbsp;</div> <div class="time">'+time+'</div></div>').fadeIn();
 	};
 };
 
@@ -93,7 +93,7 @@ function loadTasksFromCookie(){
 		var len = data.tasks.length;
 		for (var i=0; i < len; ++i){
 			// Add each task
-			var newTask = '<div class="task task'+ taskClass++ +'"><h3 contenteditable="true">' + data.tasks[i].title + '</h3><p><label name="abp">ABP: </span><input type="number" step="0.5" name="abp" class="abp" value="'+ data.tasks[i].abp + '"/></p><p><label name="abp">HP: </span><input type="number" step="0.5" name="hp" class="hp" value="'+ data.tasks[i].hp + '"/></p><div class="removeTask"><a href="#">Remove Task</a></div></div>';
+			var newTask = '<div class="task task'+ taskClass++ +'"><h3 contenteditable="true">' + data.tasks[i].title + '</h3><p><label for="abp">ABP: </span><input type="number" step="0.5" name="abp" class="abp" value="'+ data.tasks[i].abp + '"/></p><p><label for="abp">HP: </span><input type="number" step="0.5" name="hp" class="hp" value="'+ data.tasks[i].hp + '"/></p><div class="removeTask"><button class="btn"><i class="icon-remove-sign"></i></button></div></div>';
 			$(newTask).hide().appendTo('#form').fadeIn();
 			$('#estimate').hide();
 		}
@@ -105,7 +105,7 @@ $(function() {
 	// If no cookie create 2 tasks
 	if($.cookie("estimatorData") === null){
 		for(i=0; i < 2; ++i){
-			var newTask = '<div class="task"><h3 contenteditable="true">Task Title</h3><p><label name="abp">ABP: </span><input type="number" step="0.5" name="abp" class="abp" /></p><p><label name="abp">HP: </span><input type="number" step="0.5" name="hp" class="hp" /></p><div class="removeTask"><a href="#">Remove Task</a></div></div>';
+			var newTask = '<div class="task"><h3 contenteditable="true">Task Title</h3><p><label for="abp">ABP: </span><input type="number" step="0.5" name="abp" class="abp" /></p><p><label for="abp">HP: </span><input type="number" step="0.5" name="hp" class="hp" /></p><div class="removeTask"><button class="btn"><i class="icon-remove-sign"></i></button></div></div>';
 			$(newTask).hide().appendTo('#form').fadeIn();
 		}
 	// Else load cookie
@@ -115,20 +115,20 @@ $(function() {
 	
 	// Add More Tasks Button
 	$('#addTaskBtn').click(function(){
-		var newTask = '<div class="task"><h3 contenteditable="true">Task Title</h3><p><label name="abp">ABP: </span><input type="number" step="0.5" name="abp" class="abp" /></p><p><label name="abp">HP: </span><input type="number" step="0.5" name="hp" class="hp" /></p><div class="removeTask"><a href="#">Remove Task</a></div></div>';
+		var newTask = '<div class="task"><h3 contenteditable="true">Task Title</h3><p><label for="abp">ABP: </span><input type="number" step="0.5" name="abp" class="abp" /></p><p><label for="abp">HP: </span><input type="number" step="0.5" name="hp" class="hp" /></p><div class="removeTask"><button class="btn"><i class="icon-remove-sign"></i></button></div></div>';
 		$(newTask).hide().appendTo('#form').fadeIn();
 		$('#estimate').hide();
 		return false;
 	});
 	
 	// Remove Task Button
-	$('#form').on('click', '.removeTask a', function(){
+	$('#form').on('click', '.removeTask .btn', function(){
 		// Check to see there at least 2 tasks
 		if ($('.task').length > 2){
 			$(this).parent().parent().remove();
 			saveTasksToCookie();
 		} else {
-			$('#estimate').hide().html('You must have at least 2 tasks').fadeIn();
+			$('#estimate').hide().html('<div class="alert alert-error">You must have at least 2 tasks</div>').fadeIn();
 		}
 		return false;
 	});
@@ -136,9 +136,17 @@ $(function() {
 	// Enter Key Blur
 	$(".task h3, #projectTitle").keypress(function(event){
   		if (event.which == 13){
-     		$(this).blur();
+     		$(this).blur();     		
      		return false;
    		}
+	});
+
+	$('#form').on('mouseenter', '.task', function(){
+		$(this).find('.btn').css('display' , 'block')
+	});
+
+	$('#form').on('mouseleave', '.task', function(){
+		$(this).find('.btn').css('display' , 'none')
 	});
 
 	// Update Time Value
